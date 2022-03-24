478af4b4cf4a72d89cd31ff5e65bc486dc49e30e
wkava57 <wabutrune.p@hotmail.com>
2022-03-24 09:45:12 +0100

    intit

---
+++ app.js
@@ -0,0 +1,15 @@
+ 
+
+
+
+
+function addElement () {
+    // crée un nouvel élément div
+    let newDiv = document.createElement("div");
+    let newH1 = document.createElement("h1");
+    let newP = document.createElement("p");
+}
+h1.textContent = "Je fabrique de l'HTML en JS"
+p.textContent = "Et c'est pas si compliqué";
+
+div.style.backgroundColor = grey
